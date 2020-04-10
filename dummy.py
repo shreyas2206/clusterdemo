b0020735132a47fc553822c0f715b88bbb678dd8
Shreyas <shreyas2206@gmail.com>
2020-04-10 17:15:31 +0530

    init commit

---
+++ dummy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 15:53:30 2020
+
+@author: shreyasr
+"""
+
+
+import torch
+import numpy
+import time
+
+print("Startin nnet training... \n")
+
+for i in range(60):
+    time.sleep(1)
+    print("Iteration {}: loss = {}".format(i,(1/(1+i))))
+    
+print("Done")
